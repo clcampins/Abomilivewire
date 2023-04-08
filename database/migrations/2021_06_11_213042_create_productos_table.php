c441db4d09d3a8c79fd4ddcb5be7c5e53658b5b7
Carlos Campins <ccampins@itpatagonia.com>
2023-04-08 20:07:25 -0300

    primer commit

---
+++ database/migrations/2021_06_11_213042_create_productos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->text('descripcion_larga');
+            $table->string('descripcion_corta',50);
+            $table->decimal('precio_normal', 10, 2);
+            $table->decimal('precio_con_descuento', 10, 2);
+            $table->string('tipo_producto');
+            $table->string('marca');
+            $table->integer('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
